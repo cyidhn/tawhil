8eed9aea8f7d6b260662af9f6f73c6c51152652c
Jérémy DEMANGE <j@lachainemedias.com>
2020-11-03 03:52:49 +0100

    Ajout de Papaparse

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VuePapaParse from "vue-papa-parse";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VuePapaParse);
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	vuetify,
+	render: (h) => h(App),
+}).$mount("#app");
