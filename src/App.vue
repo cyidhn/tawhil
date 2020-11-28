d4fb85e74900bb801e59d2d344c51e7153deba20
Jérémy DEMANGE <j@lachainemedias.com>
2020-11-28 21:47:13 +0100

    Modifs

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,15 @@
 		<v-main>
 			<HelloWorld />
 		</v-main>
+
+		<v-card height="150">
+			<v-footer absolute class="font-weight-medium">
+				<v-col class="text-center" cols="12">
+					{{ new Date().getFullYear() }} —
+					<strong>Jérémy DEMANGE | CY IDHN</strong>
+				</v-col>
+			</v-footer>
+		</v-card>
 	</v-app>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,18 @@
 <template>
 	<v-container>
-		<v-row class="text-center">
+		<v-row class="text-left">
 			<v-col cols="12" v-if="!step2">
-				<h2 class="mb-5 headline font-weight-bold mb-3">
-					Sélectionner un fichier CSV
+				<div class="mb-12 mt-8">
+					<center>
+						<v-img
+							:src="require('../assets/Tawhil_header.png')"
+							width="750"
+							class="ct-img"
+						></v-img>
+					</center>
+				</div>
+				<h2 class="mb-5 pt-12 mt-12 headline font-weight-bold mb-3">
+					Select a CSV file
 				</h2>
 				<v-file-input
 					accept=".txt"
@@ -12,24 +21,31 @@
 					outlined
 					v-model="csvFile"
 				></v-file-input>
-				<v-btn block color="primary" @click="analyseCSV" v-if="csvFile">
-					Étape suivante
+				<v-btn block color="#FF904D" dark @click="analyseCSV" v-if="csvFile">
+					Next step
 				</v-btn>
-				<v-btn block color="primary" disabled v-else>
-					Étape suivante
+				<v-btn block color="#FF904D" disabled v-else>
+					Next step
 				</v-btn>
 			</v-col>
 			<v-col cols="12" v-if="step2">
 				<h2 class="mb-5 headline font-weight-bold mb-3">
-					Sélectionner les paramètres (*)
+					Select the parameters (*) to keep
 				</h2>
 				<div v-for="check in checkbox" :key="check.id">
 					<v-checkbox
-						class="mb-1 mt-1 pt-1 pb-1"
+						class="mb-1 mt-1 pt-1 pb-1 mt-0 mb-0"
+						color="#FF904D"
 						v-model="check.coche"
 						:label="check.name"
 					></v-checkbox>
 				</div>
+				<v-btn block color="#FF904D" dark @click="analyseCSV" v-if="csvFile">
+					Next step
+				</v-btn>
+				<v-btn block color="#FF904D" disabled v-else>
+					Next step
+				</v-btn>
 			</v-col>
 		</v-row>
 	</v-container>
@@ -37,8 +53,6 @@
 
 <script>
 	export default {
-		name: "HelloWorld",
-
 		data: () => ({
 			csvFile: null,
 			result: null,
@@ -74,3 +88,9 @@
 		},
 	};
 </script>
+
+<style scoped>
+	.ct-img {
+		border: 5px solid black;
+	}
+</style>
